3fce834e03f67eb44648b7c4ab0a7787271519f9
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-11-12 12:03:01 -0700

    feat: implemented apollo server, and typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,31 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+  _id: ID
+  username: String
+  email: String
+  password: String
+  savedBooks [Books]
+}
+
+type Book {
+  _id: ID
+  authors: String
+  description: String
+  bookId: String
+  image: String
+  link: String
+  title: String
+}
+
+type Query {
+  users: [User]
+  user(userId: ID!): User
+  me: User
+}
+
+type Mutation {
+  addProfile(name: String!, email: String!, password: String!): Auth
+  login(email: String!, password: String!): Auth
+}`;
